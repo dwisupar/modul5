596525197f6049c760f2d28152d1707fc42df121
dwisupar <38326402+dwisupar@users.noreply.github.com>
2018-04-25 22:31:35 +0700

    Delete Tugasmodul5bab6.py

--- Tugasmodul5bab6.py
+++
@@ -1,9 +0,0 @@
-print ("--PROGRAM PENJUALAN BONEKA--")
-print ("        Kelompok 39")
-print ("NAMA : Dwi Supardiyono")
-print ("NIM  : 21120117120024")
-print ("NAMA : Dimas Aldi Kartika")
-print ("NIM  : 21120117130057")
-print ("----------------------------")
-
-from input import masukan #mengimpor class masukan dan juga menampilkannya

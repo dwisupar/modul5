e2d4318620b81c7c4cbba08cbef5f1681fce51d0
dwisupar <38326402+dwisupar@users.noreply.github.com>
2018-04-25 22:41:06 +0700

    Add files via upload

---
+++ Tugasmodul5bab6.py
@@ -0,0 +1,9 @@
+print ("--PROGRAM PENJUALAN BONEKA--")
+print ("        Kelompok 39")
+print ("NAMA : Dwi Supardiyono")
+print ("NIM  : 21120117120024")
+print ("NAMA : Dimas Aldi Kartika")
+print ("NIM  : 21120117130057")
+print ("----------------------------")
+
+
